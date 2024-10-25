73203e7af34870da68d8dbde79d2c959ad802dce
Jake <launcelotraven@gmail.com>
2024-10-25 16:08:42 +0100

    merging everything into one scene

--- Assets/William/PlayerView.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-public class PlayerView : MonoBehaviour
-{
-    public Image healthBar;
-    public void UpdateHealthBar(float health) { healthBar.fillAmount = health / 100; }
-}
