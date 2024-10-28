d16a8c9de19e65927d8d97b8b09f05974679d43d
jaa3431 <jaa3431@my.acm.ac.uk>
2024-10-28 15:25:41 +0000

    PlayerModel & PlayerController
    In this commit, I added the player model and player controller.
    
    In the player model includes a float variable representing health with a GetHealth function that  can grab the variable to another script.
    
    In player Controller there is a function however is on non-edit mode (//) meaning the function won't run unless you remove all // in the void.

---
+++ Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private playerModel playerModel;
+
+    //void OnCollisionEnter(Collision other)
+    //{
+      //  Missile missile = other.gameObject.GetComponent<Missile>();
+      //  if (missile != null)
+      //  {
+            //player.Model.TakeDamage(missile.GetDamage());
+      //  }
+    //}
+}
---
+++ Assets/Scripts/Player/playerModel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerModel : MonoBehaviour
+{
+    float Health = 100;
+
+    public float GetHealth()
+    {
+        return Health;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(float Amount)
+    {
+        Health -= Amount;
+    }
+}
