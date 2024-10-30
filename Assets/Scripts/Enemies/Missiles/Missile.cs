fafd0a472337eb24649da345600bf9c47786c224
Smash_Bash <williambashford@mac.com>
2024-10-30 22:16:28 +0000

    Grabbable Missiles
    + Missiles can now be grabbed and redirected.

--- Assets/Scripts/Enemies/Missiles/Missile.cs
+++ Assets/Scripts/Enemies/Missiles/Missile.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.EventSystems.EventTrigger;
 using static UnityEngine.UI.GridLayoutGroup;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Missile : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class Missile : MonoBehaviour
     public float lifetime = 0;
     public GameObject explosionPrefab;
     public Transform aimingDummy;
+    public XRGrabInteractable grabInteractable;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -24,37 +28,40 @@ public class Missile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lifetime += Time.deltaTime;
-        if (lifetime > 2)
+        if (!grabInteractable.isSelected)
         {
-            aimingDummy.LookAt(target.transform.position);
-            transform.rotation = Quaternion.Lerp(transform.rotation, aimingDummy.rotation, Time.deltaTime * (2 + (10 * Mathf.Max(10 - Vector3.Distance(transform.position, target.position), 0))));
+            lifetime += Time.deltaTime;
+            if (lifetime > 2)
+            {
+                aimingDummy.LookAt(target.transform.position);
+                aimingDummy.Rotate(90, 0, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, aimingDummy.rotation, Time.deltaTime * (2 + (10 * Mathf.Max(10 - Vector3.Distance(transform.position, target.position), 0))));
+            }
+            else
+            {
+
+            }
+            //transform.position += transform.forward * speed * Time.deltaTime;
+            rb.velocity = transform.up * speed;
+
+
+            int LayersToIgnore = ~(1 << LayerMask.NameToLayer("No Collision"));
+            RaycastHit hit;
+            Physics.Raycast(transform.position + transform.TransformDirection(Vector3.up * 0.5f), transform.TransformDirection(Vector3.up), out hit, 2f);
+            if (hit.transform != null || Vector3.Distance(transform.position, target.position) < 0.5f)
+            {
+                Explode();
+            }
         }
         else
         {
-
-        }
-        transform.position += transform.forward * speed * Time.deltaTime;
-
-
-        int LayersToIgnore = ~(1 << LayerMask.NameToLayer("No Collision"));
-        RaycastHit hit;
-        Physics.Raycast(transform.position + transform.TransformDirection(Vector3.back * 1f), transform.TransformDirection(Vector3.forward), out hit, 2f);
-        if (hit.transform != null || Vector3.Distance(transform.position, target.position) < 0.5f)
-        {
-            Explode();
+            lifetime = -2;
         }
     }
 
     public void Explode()
     {
         GameObject newExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-        PlayerModel pm = FindObjectsOfType<PlayerModel>()[0];
-
-        if (pm ? Vector3.Distance(transform.position, pm.transform.position) < 2.5f : false)
-        {
-            pm.ReduceHealth(10);
-        }
 
         Destroy(gameObject);
     }
